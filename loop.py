5b81925c4dade488ff74355b9d9ff76d683e53b9
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-01-20 19:39:04 +0530

    loops

---
+++ loop.py
@@ -0,0 +1,28 @@
+# For Loop
+
+name = "Rajeev"
+for i in name:
+    print(i)    
+
+# ------------------------------------------------------
+
+name = "Rajeev Ravi"
+for i in range(2,7):
+    print(name[i])  
+
+# ------------------------------------------------------
+
+simpledata = " Hello John!, How is it going?"
+for i in range(2,11,2):
+    print(simpledata[i])
+
+# ------------------------------------------------------
+
+# While Loop
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+# ------------------------------------------------------
