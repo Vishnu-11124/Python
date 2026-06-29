14ce3f359ea8a76904649d28485c5b033d6a9872
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-06-29 13:21:44 +0530

    Refactor threeSum function to improve performance and handle duplicates

--- Problems/Array/three_sum.py
+++ Problems/Array/three_sum.py
@@ -1,17 +1,64 @@
 
 def threeSum(nums):
+    ans = []
     n = len(nums)
-    my_set = set()
-    for i in range(n):
-        for j in range(i+1, n):
-            for k in range(j+1, n):
-                if nums[i] + nums[j] + nums[k] == 0:
-                    temp = [nums[i],nums[j],nums[k]]
-                    temp.sort()
-                    my_set.add(tuple(temp))
+    nums.sort()
+    for i in range(n- 2):
+        if i != 0 and nums[i] == nums[i - 1]:
+            continue
 
-    return [list(ans) for ans in my_set]                
+        j = i + 1
+        k = n - 1
+        while j < k:
+            total_sum = nums[i] + nums[j] + nums[k]
+            if total_sum < 0:
+                j += 1
+            elif total_sum > 0:
+                k -= 1
+            else:
+                temp = [nums[i],nums[j],nums[k]]
+                ans.append(temp)
+                j += 1
+                k -= 1
 
+                while j<k and nums[j] == nums[j - 1]:
+                    j += 1
+                while j<k and nums[k] == nums[k + 1]:
+                    k -= 1
+
+    return ans                
+
+# ----------------------------------------------------------
+
+# def threeSum(nums):
+#     n = len(nums)
+#     result = set()
+
+#     for i in range(n):
+#         my_set = set()
+#         for j in range(i+1, n):
+#             third = -(nums[i]+nums[j])
+#             if third in my_set:
+#                 temp = [nums[i], nums[j], third]
+#                 temp.sort()
+#                 result.add(tuple(temp))
+#             my_set.add(nums[j])
+#     return [list(ans) for ans in result]         
+
+# ----------------------------------------------------------
+
+# def threeSum(nums):
+#     n = len(nums)
+#     my_set = set()
+#     for i in range(n):
+#         for j in range(i+1, n):
+#             for k in range(j+1, n):
+#                 if nums[i] + nums[j] + nums[k] == 0:
+#                     temp = [nums[i],nums[j],nums[k]]
+#                     temp.sort()
+#                     my_set.add(tuple(temp))
+
+#     return [list(ans) for ans in my_set]                
 
 # ----------------------------------------------------------
 
