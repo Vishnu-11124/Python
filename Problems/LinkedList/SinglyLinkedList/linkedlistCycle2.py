d373192a047de1773aef07d35197dd16bc6280f6
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-07-10 21:56:17 +0530

    Add ListNode class and hasCycle method to detect cycles in a linked list

---
+++ Problems/LinkedList/SinglyLinkedList/linkedlistCycle2.py
@@ -0,0 +1,32 @@
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        slow = head
+        fast = head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                slow = head
+                index = 0
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                    index += 1
+                return slow                 
+
+        return None
+
+head = ListNode(3)
+head.next = ListNode(2)
+head.next.next = ListNode(0)
+head.next.next.next = ListNode(-4)
+head.next.next.next.next = head.next
+
+sol = Solution()
+print(sol.hasCycle(head))
